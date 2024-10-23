95a8d95aa52fca6d9adb135a81bd7e13566c9682
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-23 11:46:19 -0400

    Phase 19

--- src/hooks/useWheather.ts
+++ src/hooks/useWheather.ts
@@ -1,7 +1,17 @@
+import axios from "axios"
+import { SearchType } from "../types"
 
 export default function useWheather() {
-    const fetchWheather = () => {
-        console.log('Finding out...')
+    const fetchWheather = async (search: SearchType) => {
+        try {
+            const appId = '1f00bbf97ba70a8b959c6c30b1e50ba3'
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+
+            const { data } = await axios(geoUrl)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
     }
   return {
     fetchWheather
