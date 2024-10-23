e1fa367e2db052ab19bacf88f69bd6afcb7df188
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-23 11:08:09 -0400

    Phase 16

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import styles from "./App.module.css"
 import Form from "./components/Form/Form"
+import useWheather from "./hooks/useWheather"
 
 function App() {
+
+  const { fetchWheather } = useWheather()
 
   return (
     <>
       <h1 className={styles.title}>Wheather Searcher</h1>
 
       <div className={styles.container}>
-        <Form/>
+        <Form
+          fetchWheather={fetchWheather}
+        />
         <p>2</p>
       </div>
     </>
