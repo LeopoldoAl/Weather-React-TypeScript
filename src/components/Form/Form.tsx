fab04568751c37320a7496816651473bcc7446be
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-23 09:59:42 -0400

    Phase 12

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,21 @@
+import { ChangeEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
+import type { SearchType } from "../../types";
 
 export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const handleChange = (e : ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
   return (
     <form className={styles.form}>
 
@@ -12,12 +26,19 @@ export default function Form() {
                 type="text" 
                 name="city"
                 placeholder="City"
+                value={search.city}
+                onChange={handleChange}
             />
         </div>
 
         <div className={styles.field}>
-            <label htmlFor="city">Country:</label>
-             <select>
+            <label htmlFor="country">Country:</label>
+             <select
+                id="country"
+                value={search.country}
+                name="country"
+                onChange={handleChange}
+             >
                     <option>-- Select a Country --</option>
                     {countries.map( country => (
                         <option
