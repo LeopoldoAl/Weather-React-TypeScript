7f0d3f4ee24e006f3998db150a9c8db7c319cc96
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-24 12:39:41 -0400

    Phase 29

---
+++ src/components/WeatherDetails/WeatherDetails.tsx
@@ -0,0 +1,14 @@
+import { Weather } from "../../hooks/useWheather"
+
+type WeatherDetailsProps = {
+    weather: Weather
+}
+
+export default function WeatherDetails({weather}: WeatherDetailsProps) {
+  return (
+    <div>
+        Weather from:
+        
+    </div>
+  )
+}
