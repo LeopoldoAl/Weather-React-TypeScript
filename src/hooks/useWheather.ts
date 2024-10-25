b3c1aae86dd48a0bcc1ca7b9e413db23dcbe2bd3
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-25 10:14:42 -0400

    Phase 42

--- src/hooks/useWheather.ts
+++ src/hooks/useWheather.ts
@@ -15,38 +15,49 @@ const weatherSchema = object({
 
 export type Weather =InferOutput<typeof weatherSchema>
 
+const initialState = {
+  name: '',
+  main: {
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0
+  }
+}
+
 export default function useWheather() {
 
-            const [weather, setWeather] = useState<Weather>({
-              name: '',
-              main: {
-                temp: 0,
-                temp_max: 0,
-                temp_min: 0
-              }
-            })
+            const [weather, setWeather] = useState<Weather>(initialState)
 
             const [loading, setLoading] = useState(false)
 
     const fetchWheather = async (search: SearchType) => {
       const appId = import.meta.env.VITE_API_KEY
       setLoading(true)
+      setWeather(initialState)
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
             const { data } = await axios(geoUrl)
             
-            const lat = data[0].lat
-            const lon = data[0].lon
-
-            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
-
-            const { data: weatherResult } = await axios(weatherUrl)
-
-            const result = parse(weatherSchema, weatherResult)
-
-            if (result) {
-              setWeather(result)
+            if(!data[0]) {
+              setWeather(initialState)
+              return
+            }
+            if (data[0].name.trim()===search.city.trim() && data[0].country.trim()===search.country.trim()) {
+              const lat = data[0].lat
+              const lon = data[0].lon
+  
+              const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+  
+              const { data: weatherResult } = await axios(weatherUrl)
+  
+              const result = parse(weatherSchema, weatherResult)
+  
+              if (result) {
+                setWeather(result)
+            } else {
+              setWeather(initialState)
+            }
             }
 
             
